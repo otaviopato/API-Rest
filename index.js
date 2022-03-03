51367a8be1201a020b44f01996c8881625f5a91b
Otávio Pato <61769935+otaviopato@users.noreply.github.com>
2022-03-03 07:31:21 -0300

    Substituindo body-parser e add cadastro de jogos

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 var DB = {
     games: [
